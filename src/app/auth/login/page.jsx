dd05e45b1870ef1ba9bc67a06289eec26cbffe4b
Swatantra <54662673+swatantragoswami09@users.noreply.github.com>
2025-12-02 20:03:31 +0530

    added 5 attempt check (#25)
    * added 5 attempt check
    
    * removed unused code
    
    * refactor code
    
    * small fix
    
    * - fixed vulnerabilities
    
    ---------
    
    Co-authored-by: Swatantra kumar goswami <swatantrakumargoswami@Swatantras-MacBook-Pro.local>
    Co-authored-by: Divyang Khatri <divyangkhatri.dk@gmail.com>

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -1,11 +1,5 @@
 'use client';
-import React, {
-  useState,
-  useEffect,
-  useCallback,
-  useContext,
-  useRef,
-} from 'react';
+import React, {useState, useCallback, useRef} from 'react';
 import {Formik} from 'formik';
 import styles from './LoginScreen.module.css';
 import FormControl from '@mui/material/FormControl';
@@ -18,31 +12,27 @@ import IconButton from '@mui/material/IconButton';
 import {validationSchemaLogin} from 'utils/validationSchema';
 import ModalReset from 'components/ModalReset';
 import {useRouter, useSearchParams} from 'next/navigation';
-import {VerifyInfoModal} from 'components/VerifyInfo';
-// import {
-//   logInSuccess,
-//   fingerprintAuthSuccess,
-//   loadingOff,
-//   loadingOn,
-// } from 'dok-wallet-blockchain-networks/redux/auth/authSlice';
-//
-// import {validationSchemaLogin} from 'utils/validationSchema';
-// import {isFingerprint} from 'dok-wallet-blockchain-networks/redux/settings/settingsSelectors';
-// import FingerprintScanner from 'react-native-fingerprint-scanner';
-// import {IS_IOS, useFloatingHeight} from 'dok-wallet-blockchain-networks/service/dimensions';
-// import {ThemeContext} from '../../../../ThemeContext';
-// import myStyles from './LoginScreenStyles';
 import {
+  handleAttempts,
   loadingOff,
   logInSuccess,
-  logOutSuccess,
+  resetAttempts,
+  setLastAttempt,
 } from 'dok-wallet-blockchain-networks/redux/auth/authSlice';
 import {useDispatch, useSelector} from 'react-redux';
-import {getUserPassword} from 'dok-wallet-blockchain-networks/redux/auth/authSelectors';
+import {
+  getAttempts,
+  getIsLocked,
+  getLastAttempt,
+  getMaxAttempt,
+  getUserPassword,
+} from 'dok-wallet-blockchain-networks/redux/auth/authSelectors';
 import {selectAllWallets} from 'dok-wallet-blockchain-networks/redux/wallets/walletsSelector';
 import {refreshCoins} from 'dok-wallet-blockchain-networks/redux/wallets/walletsSlice';
 import {getAppSubTitle} from 'whitelabel/whiteLabelInfo';
-// import RNScreenshotPrevent from 'react-native-screenshot-prevent';
+import {isWalletReset} from 'dok-wallet-blockchain-networks/redux/settings/settingsSelectors';
+import ModalInfo from 'src/components/ModalInfo';
+import {Constants} from 'src/utils/common';
 
 const LoginScreen = () => {
   const [hide, setHide] = useState(true);
@@ -50,117 +40,32 @@ const LoginScreen = () => {
   const [wrong, setWrong] = useState(false);
   const router = useRouter();
   const buttonRef = useRef();
-
-  //   const {theme} = useContext(ThemeContext);
-  //   const styles = myStyles(theme);
-
   const dispatch = useDispatch();
-  //   const [wrong, setWrong] = useState(false);
   const storePassword = useSelector(getUserPassword);
-  //   const fingerprint = useSelector(isFingerprint);
-  //   const isFinger = useSelector(getFingerprintAuth);
-  //   const floatingBtnHeight = useFloatingHeight();
   const allWallets = useSelector(selectAllWallets);
+  const rateLimitCheck = useSelector(isWalletReset);
+  const lastAttempt = useSelector(getLastAttempt);
   const searchParams = useSearchParams();
 
-  //   useEffect(() => {
-  //     RNScreenshotPrevent.enabled(true);
-  //     if (IS_IOS && !__DEV__) {
-  //       RNScreenshotPrevent.enableSecurediv();
-  //     }
-  //     return () => {
-
-  //     };
-  //   }, []);
-
-  //   useEffect(() => {
-  //     dispatch(loadingOff());
-  //     setTimeout(() => {
-  //       handleFingerprintAuth();
-  //     }, 200);
-  //   }, [dispatch, handleFingerprintAuth]);
-
-  //   useEffect(() => {
-  //     if (!isFinger) {
-  //       handleFingerprintAuth();
-  //     }
-  //   }, [isFinger, handleFingerprintAuth]);
+  const attempts = useSelector(getAttempts);
+  const isLocked = useSelector(getIsLocked);
+  const MAX_ATTEMPT = useSelector(getMaxAttempt);
 
   const hasWallet = useCallback(() => {
     return allWallets?.length !== 0;
   }, [allWallets]);
 
-  //   const handleFingerprintAuth = useCallback(async () => {
-  //     if (fingerprint) {
-  //       try {
-  //         const isAuth = await FingerprintScanner.authenticate({
-  //           description: 'Unlock Dok Wallet with your fingerprint',
-  //         });
-
-  //         dispatch(fingerprintAuthSuccess(isAuth));
-  //         if (hasWallet()) {
-  //           console.log('hasWallet: in login', hasWallet());
-  //           dispatch(loadingOn());
-  //           setTimeout(() => {
-  //             navigation.reset({
-  //               index: 0,
-  //               routes: [{name: 'Sidebar'}],
-  //             });
-  //             dispatch(loadingOff());
-  //           }, 200);
-  //         } else {
-  //           dispatch(loadingOn());
-  //           navigation.reset({
-  //             index: 0,
-  //             routes: [{name: 'ResetWallet', params: {isFromOnBoarding: true}}],
-  //           });
-  //         }
-  //       } catch (error) {
-  //         if (error.name === 'SystemCancel') {
-  //           console.log('Authentication was canceled by the system');
-  //         } else {
-  //           console.log('Error checking fingerprint settings:', error);
-  //         }
-  //       } finally {
-  //         FingerprintScanner.release();
-  //       }
-  //     }
-  //   }, [dispatch, fingerprint, navigation, hasWallet]);
-
-  //   const handleSubmit = values => {
-  //     Keyboard.dismiss();
-  //     if (storePassword === values.password) {
-  //       console.log('Login successful');
-  //       dispatch(fingerprintAuthSuccess(true));
-  //       dispatch(logInSuccess(values.password));
-  //       if (hasWallet()) {
-  //         console.log('hasWallet: in login', hasWallet());
-  //         dispatch(loadingOff());
-  //         navigation.reset({
-  //           index: 0,
-  //           routes: [{name: 'Sidebar'}],
-  //         });
-  //       } else {
-  //         navigation.reset({
-  //           index: 0,
-  //           routes: [{name: 'ResetWallet', params: {isFromOnBoarding: true}}],
-  //         });
-  //       }
-  //     } else {
-  //       console.log('Wrong password');
-  //       setWrong(true);
-  //       dispatch(loadingOff());
-  //     }
-  //   };
-  /////////////////////////
   const handleReset = useCallback(() => {
     setShowResetModal(true);
   }, []);
 
   const onClickLogin = useCallback(
-    values => {
+    async values => {
       if (storePassword === values.password) {
         dispatch(logInSuccess(values.password));
+        if (rateLimitCheck) {
+          dispatch(resetAttempts());
+        }
         if (hasWallet()) {
           const redirectRoute = searchParams?.get('redirectRoute');
           let searchParamsString = '';
@@ -180,12 +85,16 @@ const LoginScreen = () => {
         } else {
           router.replace('/auth/reset-wallet');
         }
+      } else if (rateLimitCheck) {
+        dispatch(handleAttempts({router}));
+        setWrong(true);
+        dispatch(loadingOff());
       } else {
         setWrong(true);
         dispatch(loadingOff());
       }
     },
-    [dispatch, hasWallet, router, searchParams, storePassword],
+    [dispatch, hasWallet, rateLimitCheck, router, searchParams, storePassword],
   );
 
   const onKeyDown = useCallback(e => {
@@ -302,6 +211,12 @@ const LoginScreen = () => {
           </button>
         </div>
       </div>
+      <ModalInfo
+        visible={lastAttempt}
+        title={Constants.lastAttempt.title}
+        message={Constants.lastAttempt.subTitle}
+        handleClose={() => dispatch(setLastAttempt(false))}
+      />
 
       <ModalReset
         visible={showResetModal}
